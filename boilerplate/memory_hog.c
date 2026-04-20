73778296fc26dd33d9faa364307c3282bff1455a
ryonahh <ryonaghosh06@gmail.com>
2026-04-20 11:48:46 +0000

    Update memory_hog.c

--- boilerplate/memory_hog.c
+++ boilerplate/memory_hog.c
@@ -3,8 +3,7 @@
 
 int main() {
     while (1) {
-        void *p = malloc(10 * 1024 * 1024);
-        if (!p) break;
-        memset(p, 0, 10 * 1024 * 1024);
+        void *p = malloc(10*1024*1024);
+        memset(p, 0, 10*1024*1024);
     }
 }
